a265f69d297fbe67965928668446f3d392cd6f91
Maciej Kalytczak <maciek@kamac.com.pl>
2018-09-01 00:05:24 +0200

    API Setup

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Table }  from 'react-bootstrap';
+
+import Profile from './Profile';
+import Slider from './Slider';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTab : 'profile'
+    } 
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Kamac nauka!</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#"  onClick={() => this.setState({currentTab:'slider'})}>Slider</NavItem>
+            <NavItem eventKey={2} href="#" onClick={() => this.setState({currentTab:'profile'})}>Profile</NavItem>
+          </Nav>
+        </Navbar>
+        <div className="container">
+        {this.state.currentTab==='slider' ? <Slider /> : false }
+        {this.state.currentTab==='profile' ? <Profile /> : false }
+        </div>
+     </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+
+class Profile extends Component {
+    
+
+    componentDidMount() {
+        let header = new Headers({"Content-Type":"application/json","Authorization":"token 65dc0c6779b2c89855cf597907ad9a97820c8dc8"});
+        fetch('https://api.github.com/users/macrust', {
+
+        method: 'GET',
+        headers: header
+        
+    })
+    .then(response => response.json()).then(json => {console.log(json)}).catch(error => {console.log(error)});
+
+    
+      //65dc0c6779b2c89855cf597907ad9a97820c8dc8
+      }
+
+  render() {
+    return (
+      <div>
+        Profile page
+     </div>
+    );
+  }
+}
+
+export default Profile;
